0fdfa91da0c40744b87ace059a985ba8684e6852
Lucas <lucashenriquecardosoprado@gmail.com>
2025-08-27 21:39:57 -0300

    Criando a classe conexão

---
+++ SistemaPizzaria/Conexao.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPizzaria
+{
+    class Conexao
+    {
+        MySqlConnection con = new MySqlConnection("Data Source=localhost;Initial Catalog=BDPizzaria;user=root;pwd=12345678");
+
+        public static string msg;
+
+        public MySqlConnection ConnectarBD()
+        {
+            try
+            {
+                con.Open();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return con;
+        }
+
+        public MySqlConnection DesConnectarBD()
+        {
+            try
+            {
+                con.Close();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return con;
+        }
+    }
+}
