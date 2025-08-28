ffc3fc9f92e999b9fab0f0bb7a90dedb9ca21dff
Lucas <lucashenriquecardosoprado@gmail.com>
2025-08-27 21:35:11 -0300

    Criando o Form Pedido

--- SistemaPizzaria/Pedido.cs
+++ SistemaPizzaria/Pedido.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SistemaPizzaria
 {
-    public partial class Form1 : Form
+    public partial class Pedido : Form
     {
-        public Form1()
+        public Pedido()
         {
             InitializeComponent();
         }
